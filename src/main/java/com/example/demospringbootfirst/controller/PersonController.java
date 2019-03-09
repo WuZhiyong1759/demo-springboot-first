bedc6c024cde0854dd11c078e8dca5bf36666026
wuzhiyong <563653092@qq.com>
2019-03-09 13:23:09 +0800

    study-01

--- src/main/java/com/example/demospringbootfirst/controller/PersonController.java
+++ src/main/java/com/example/demospringbootfirst/controller/PersonController.java
@@ -5,7 +5,6 @@ import com.example.demospringbootfirst.service.PersonService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 @RestController
 public class PersonController {
